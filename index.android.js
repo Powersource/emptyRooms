41185603538ae5d6f2afcefd1de1781cd94051ba
shamiran <shamiran.jaf@gmail.com>
2016-10-16 19:51:38 +0200

    Add static react components for RoomTable, RoomRow

--- index.android.js
+++ index.android.js
@@ -19,7 +19,7 @@ export default class emptyRooms extends Component {
         <Text style={styles.welcome}>
           Welcome to emptyRooms!
         </Text>
-        <FilterableRoomTable roomType ='exercise' />
+        <FilterableRoomTable />
       </View>
     );
   }
@@ -28,18 +28,52 @@ export default class emptyRooms extends Component {
 class FilterableRoomTable extends Component {
   render() {
     return (
-      <Text style={styles.welcome}>
-        Available {this.props.roomType}rooms:
-      </Text>
+      <View>
+        <Text style={styles.welcome}>
+          Available rooms:
+        </Text>
+        <RoomTable />
+      </View>  
       );
   }
 };
+class RoomTable extends Component {
+  render() {
+    return (
+      <View>
+        <View style={styles.descriptionBar}>
+          <Text>
+            Room
+          </Text>
+          <Text>
+            Available   
+          </Text>    
+        </View>
+        <View style={styles.roomList}>
+          <RoomRow roomName='E:3316' roomTime='13:00-15:00' />
+        </View>
+      </View>
+
+      );
+  }
+};
+class RoomRow extends Component {
+  render() {
+    return (
+      <View style={styles.roomRow}>
+        <Text>{this.props.roomName}</Text>
+        <Text>{this.props.roomTime}</Text>
+      </View>
+    );
+  }
+}
+
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -53,6 +87,22 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  descriptionBar: {
+    flexDirection:'row',
+    justifyContent: 'space-around',
+  },
+  roomList: {
+
+  },
+  roomRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
 });
+
+var ROOMS = [
+{roomName:'E:3316', roomTime:'08:00-17:00'},
+{roomName='E:3317', roomTime='09:00-17:00'},
+{roomName='E:3318', roomTime='10:00-17:00'}];
 
 AppRegistry.registerComponent('emptyRooms', () => emptyRooms);
