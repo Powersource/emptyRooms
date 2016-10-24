cb439a9d1a58317d98541e7a10c9c3ea49c1bb3b
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-10-24 22:54:56 +0200

    Add the day picker dropdown list
    Jesus fucking christ this took its sweet time

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Rooms from './rooms.js';
 class App extends Component {
 	constructor () {
 		super()
-		this.state = {ROOMS: [{roomName:'E:3318', roomTime:'16:00-17:00'}]}
+		this.state = {ROOMS: [{roomName:'E:3318', roomTime:'16:00-17:00'}],
+		day: "0"}
 		new Rooms().then( (data) => {
 			this.setState({ROOMS: data.getAllRoomInfo()});
 		});
+		this.selectDay = this.selectDay.bind(this);
 	  }
   render() {
     return (
@@ -17,20 +19,33 @@ class App extends Component {
         <h1 className="App-header">
           Welcome to emptyRooms!
         </h1>
+	<div>
+		<select name="day" id="day" value={this.state.day} onChange={this.selectDay}>
+			// I'm not gonna try to do this shit with a loop again
+			// Or maybe, now that I don't seem to be allowed to use 'selected'
+			<option value="-1">-1</option>
+			<option value="0">0</option>
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+		</select>
+	</div>
         <FilterableRoomTable roomTable={this.state.ROOMS}/>
       </div>
-      
     );
   }
-
+  selectDay(event) {
+	  this.setState({day: event.target.value});
+  }
 }
 class FilterableRoomTable extends Component {
 
   render() {
     return (
       <div className="FilterableRoomTable">
-        
-        
         <RoomTable roomTable={this.props.roomTable} />
       </div>  
       );
